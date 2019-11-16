9dd3e6d9804fc2adb4625e65f198d1dde6979aa6
lucapu88 <caputoluca88@gmail.com>
2019-11-16 16:20:24 +0100

    abbellimento in html e css

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+//Attraverso un prompt chiedere all'utente la sua email.
 var email = prompt('Ciao! inserisci la tue email e verifichiamo se sei in lista.');
 console.log(email);
+//Creare un array contenente una lista di email.
 var iscritti = ['matteo@gmail.com', 'luca@gmail.com', 'sofia@gmail.com', 'antonio@hotmail.com', 'franco@gmail.com'];
 console.log(iscritti);
+//Controllare se l'email dell'utente è presente nell'array e stampare un messaggio appropriato.
 for (var i = 0; i < iscritti.length; i++) {
   if (email == iscritti[i]) {
-    document.getElementById('ok').innerHTML = 'ok sei in lista';
+    document.getElementById('ok').innerHTML = 'Fantastico sei in lista! Ci vediamo al concerto!';
   }
 }
+
+document.getElementById('stampa-risultato').setAttribute('class','visible');
